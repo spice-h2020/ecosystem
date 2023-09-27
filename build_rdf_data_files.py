4127f2c0b556798add8cdf3ad8301c1eb3e2b978
Enrico Daga <enrico.daga@open.ac.uk>
2023-09-27 18:03:18 +0100

    Moved schema to jsonld, embedded jsonld in component page

--- build_rdf_data_files.py
+++ build_rdf_data_files.py
@@ -4,15 +4,13 @@ import shutil
 
 engine = pysa.SparqlAnything()
 directory = './content/'
-if os.path.exists('./rdf/'):
-    shutil.rmtree('./rdf/')
 for root, dirs, files in os.walk(directory):
     for filename in files:
         location = os.path.join(root, filename)
         print(location)
         pre, ext = os.path.splitext(location)
-        pre = pre.replace("./content/", "./rdf/")
-        output = pre + ".schema.ttl"
+        #pre = pre.replace("./content/", "./rdf/")
+        output = pre + ".schema.json"
         pth = os.path.dirname(os.path.abspath(output))
         if not os.path.exists(pth):
             os.makedirs(pth)
